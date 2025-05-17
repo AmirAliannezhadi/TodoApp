ab4a7843789304a7569f73d370200dc94736bfb4
AmirAliannezhadi <amiraliannezhad.dev@gmail.com>
2025-05-17 19:04:42 +0330

    signup jsx done

---
+++ components/template/SignupPage.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+function SignupPage() {
+  return <div className="signin-form">
+    <h3>Registration Form</h3>
+    <input type="text" placeholder="Email" />
+    <input type="password" placeholder="Password" />
+    <button>Register</button>
+    <div>
+        <p>Have an account?</p>
+        <Link href="/signin">Sign in</Link>
+    </div>
+  </div>;
+}
+
+export default SignupPage;
