ceb059705865f1b5c4962705865bbfa7a331ae88
AmirAliannezhadi <amiraliannezhad.dev@gmail.com>
2025-05-19 18:19:00 +0330

    update butoon done

--- components/module/Tasks.js
+++ components/module/Tasks.js
@@ -1,13 +1,26 @@
+import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 import { RiMastodonLine } from "react-icons/ri";
 
-function Tasks({ data }) {
+function Tasks({ data, fetchTodos, next, back }) {
   return (
     <div className="tasks">
       {data?.map((i) => (
         <div key={i._id} className="tasks__card">
-            <span className={i.status}></span>
-            <RiMastodonLine />    
-            <h4>{i.title}</h4>
+          <span className={i.status}></span>
+          <RiMastodonLine />
+          <h4>{i.title}</h4>
+          <div>
+            {back ? (
+              <button className="button-back">
+                <BiLeftArrow /> Back
+              </button>
+            ) : null}
+            {next ? (
+              <button className="button-next">
+                Next <BiRightArrow />
+              </button>
+            ) : null}
+          </div>
         </div>
       ))}
     </div>
