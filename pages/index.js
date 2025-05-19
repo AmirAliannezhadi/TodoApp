88fe1b6a173eeff11ce4a867a7bdafe5424d581e
AmirAliannezhadi <amiraliannezhad.dev@gmail.com>
2025-05-19 21:30:21 +0330

    done

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import { getSession } from "next-auth/react";
 import HomePage from "../components/template/HomePage";
 
 export default function Home() {
-  return <HomePage />
+  return <HomePage />;
+}
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  if (!session) {
+    return {
+      redirect: { destination: "/signin", permanent: false },
+    };
+  } else {
+    return {
+      props: {},
+    };
+  }
 }
